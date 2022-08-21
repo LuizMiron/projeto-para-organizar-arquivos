f18b6fe8ed48c17542fa79db7d991350d99ddab1
LuizMiron <luiz.gugamiron@gmail.com>
2022-08-21 13:00:12 -0300

    funcao da extensao

--- organizando.py
+++ organizando.py
@@ -6,9 +6,15 @@ def nome_arquivos(diretorio):
     arquivos = os.listdir(diretorio)
 
     for arq in arquivos:
-        pass
-    
-# 2. Pegar sua extensão para deternimar o seu tipo
+        ext_arquivos(arq)
 
+# 2. Pegar sua extensão para deternimar o seu tipo
+def ext_arquivos(arquivo):
+    if arquivo.count('.') > 0:
+        est = arquivo.split('.')
+        print(est[-1])
+    else:
+        print('é uma pasta!')
 # 3. Criar pastas: imagens, audios e documentos obs: pasta "outros" já existe
 # 4. Mover determinado arquivo nas pastas, baseado no seu tipo
+nome_arquivos('arquivos')
