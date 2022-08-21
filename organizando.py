fc84e2a9fdc606e3c5c16462903db2a9f751ba43
LuizMiron <luiz.gugamiron@gmail.com>
2022-08-21 12:48:04 -0300

    funcao nome arquivos

--- organizando.py
+++ organizando.py
@@ -1,1 +1,14 @@
 import os
+
+
+# 1. Pegar o nome dos arquivos
+def nome_arquivos(diretorio):
+    arquivos = os.listdir(diretorio)
+
+    for arq in arquivos:
+        pass
+    
+# 2. Pegar sua extensão para deternimar o seu tipo
+
+# 3. Criar pastas: imagens, audios e documentos obs: pasta "outros" já existe
+# 4. Mover determinado arquivo nas pastas, baseado no seu tipo
