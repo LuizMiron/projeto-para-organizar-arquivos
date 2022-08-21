d5011b237bfbe3d0834eb5322541c8d55e263ded
LuizMiron <luiz.gugamiron@gmail.com>
2022-08-21 13:55:30 -0300

    funcao criar pastas

--- organizando.py
+++ organizando.py
@@ -1,20 +1,28 @@
 import os
 
+# 1. Criar pastas: imagens, audios e documentos obs: pasta "outros" já existe
+def criar_pastas(diretorio):
+    AUDIOS_DIR = os.path.join(diretorio, 'audios')
+    IMAGENS_DIR = os.path.join(diretorio, 'imagens')
+    DOCS_DIR = os.path.join(diretorio, 'documentos')
+    VIDEOS_DIR = os.path.join(diretorio, 'videos')
+    OUTROS_DIR = os.path.join(diretorio, 'outros')
 
-# 1. Pegar o nome dos arquivos
-def nome_arquivos(diretorio):
+
+# 2. Pegar o nome dos arquivos
+def organizar(diretorio):
     arquivos = os.listdir(diretorio)
 
     for arq in arquivos:
-        ext_arquivos(arq)
+        extensao = ext_arquivos(arq)
 
-# 2. Pegar sua extensão para deternimar o seu tipo
+# 3. Pegar sua extensão para deternimar o seu tipo
 def ext_arquivos(arquivo):
     if arquivo.count('.') > 0:
-        est = arquivo.split('.')
-        print(est[-1])
+        index = arquivo.rfind('.')
+        return arquivo[index:]
     else:
-        print('é uma pasta!')
-# 3. Criar pastas: imagens, audios e documentos obs: pasta "outros" já existe
+        pass
+# 1. Criar pastas: imagens, audios e documentos obs: pasta "outros" já existe
 # 4. Mover determinado arquivo nas pastas, baseado no seu tipo
-nome_arquivos('arquivos')
+organizar('arquivos')
